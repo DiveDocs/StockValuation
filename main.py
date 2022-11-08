751711cea60c83f1dbd6076084d1673b51497d25
DiveDocs <divedoc@yahoo.com>
2022-11-08 19:22:40 +0100

    Retrieved most basic information

--- main.py
+++ main.py
@@ -1,24 +1,70 @@
+
 import pandas as pd
 import yfinance as yf
+def getFundamentals(df, str):
+    dataList = df[str]  #type is of type string, either Revenue or Earnings
+
 
 def getStockData(symbol):
+    print(symbol)
     ticker = yf.Ticker(symbol)
     #print(ticker)
-    print(symbol)
-    print('CurrentPrice:',ticker.info['currentPrice'])
-    #print(ticker.info['zip']
+    df = ticker.info
+    print(df['fiftyTwoWeekLow'])
+    #df = pd.DataFrame.from_dict(dict, orient='index')
+    #df = df.reset_index()
+    #print(df)
+
+    #Now grab all kind of relevant information we need later on
+    low52 = df['fiftyTwoWeekLow']
+    high52 = df['fiftyTwoWeekHigh']
+    curr = df['currentPrice']
+
+    print('52L: ', low52,  '/ Current: ', curr, '/ 52H', high52)
+    cf = df['operatingCashflow']
+    dbt = df['totalDebt']
+    tc = df['totalCash']
+    fpe = df['forwardEps']
+    tpe = df['trailingEps']
+    print('CashFlow: ', '${:,.2f}'.format(cf),  'vs. Debt: ', '${:,.2f}'.format(dbt), ': ', "{:.2%}". format(cf/dbt))
+    print('Cash: ', '${:,.2f}'.format(cf),  'vs. Debt: ', '${:,.2f}'.format(dbt), ': ', "{:.2%}". format(tc/dbt))
+    fpe = df['forwardEps']
+    tpe = df['trailingEps']
+    print('forwardEps: ', fpe,  'vs. trailingEPS: ', tpe)
+    ldv = df['lastDividendValue']
+    if ldv:
+        print('Dividend/CurrentPrice: ', "{:.2%}".format(ldv/curr))
+    else:
+        print('No Dividend reported')
+    peg = df['pegRatio']
+    print('PEG Ratio: ', peg)
+
+
     df=ticker.earnings
     revenueList = df['Revenue']
     print('Revenue')
-   #print(df['Revenue'])
+    # print(df['Revenue'])
     for i in range(4):
         revnow = revenueList.iloc[i]
-        #print(revnow)
+        # print(revnow)
         if (i > 0):
-            revpast = revenueList.iloc[i-1]
-            print('Revenue: ', revnow, 'Growth: ', ((revnow/revpast)-1)*100, '%')
+            revpast = revenueList.iloc[i - 1]
+            print('Revenue: ', '${:,.2f}'.format(revnow), 'Growth: ', "{:.2%}". format((revnow / revpast) - 1))
         else:
-            print('Revenue: ', revnow)
+            print('Revenue: ', '${:,.2f}'.format(revnow))
+    earningsList = df['Earnings']
+    print('Earnings')
+    # print(df['Earnings'])
+    for i in range(4):
+        earnnow = earningsList.iloc[i]
+        # print(revnow)
+        if (i > 0):
+            earnpast = earningsList.iloc[i - 1]
+            print('Earning: ', '${:,.2f}'.format(earnnow), 'Growth: ', "{:.2%}". format((earnnow / earnpast) - 1))
+        else:
+            print('Earning: ', '${:,.2f}'.format(earnnow))
+
+    df=ticker.get_cashflow()
     print('------------------------------------------------------------------------------------')
 
 # Press the green button in the gutter to run the script.
